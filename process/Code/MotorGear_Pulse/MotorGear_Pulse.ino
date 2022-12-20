db27b7ffaaba62b7ea7a350279df65dd14b39ecc
DorianJov <116266023+DorianJov@users.noreply.github.com>
2022-12-21 00:25:36 +0100

    CodeForTestDay
    - Added Vibration hint lvl 1
    - added rules for lvl 1
    - added led feedback for level 1
    
    To Test:
    - led systeme is enough ?
    (green,orange,red) or only (green red)
    - Vibration does really help ?

--- process/Code/MotorGear_Pulse/MotorGear_Pulse.ino
+++ process/Code/MotorGear_Pulse/MotorGear_Pulse.ino
@@ -45,6 +45,11 @@ int whichLevel = 0;
 
 unsigned long LEDpreviousMillis = 0;
 unsigned long VibratorpreviousMillis = 0;
+unsigned long VibratorpreviousMillis2 = 0;
+
+unsigned long isMovingpreviousMillis = 0;
+
+unsigned long IsStoppedpreviousSeconds = 0;
 
 
 //motor+
@@ -68,7 +73,12 @@ bool gameJustStarted = true;
 
 bool hintIsfinished = false;
 bool delayIsPassed = false;
+bool userskippedHint = false;
+bool ismoving = false;
 
+int LVL1Steps = 0;
+
+bool stepIsConfirmed = false;
 void setup() {
 
   //ESP.restart();
@@ -90,7 +100,7 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(String("Result:  ") + Count_pulses + String(" CurrentLevel: ") + whichLevel);
+  Serial.println(String(" isMoving ?  ") + isMoving(false) + String("  Is stopped for 5 sec:  ") + isStopforXsec(5000) + String("  Result:  ") + Count_pulses + String(" CurrentLevel: ") + whichLevel);
 
   //LevelManager(0);
 
@@ -157,6 +167,7 @@ void start() {
 
   if (MotorTriggerCount == 1) {
     //pass to level
+    Count_pulses = 0;
     whichLevel = 1;
     MotorTriggerCount = 0;
   }
@@ -176,33 +187,182 @@ void LevelManager(int Level) {
 
 void levelONE() {
 
-
+  Serial.print(String("Vibration sequence:    ") + vibrationSequence);
   //check start function to help
   // LevelONE Hint.
   //if motor isnt moving show hint.
   //if motor is moving cancel hint and resetMotor.
+
+
   if (!hintIsfinished) {
-    led(blue, light, 0);
     switch (vibrationSequence) {
+
+      //needed to create a delay on the first vibration(case 0).
+      //case -1 doesn't do anything appart to make the first vibration not going instant.
+      case -1:
+        setVibration(0, 0);
+        break;
+
       case 0:
-        setVibration(250, 100);
+        led(blue, light, 0);
+        setVibration(800, 80);
         break;
       case 1:
-        setVibration(1000, 255);
+        led(blue, light, 0);
+        setVibration(500, 80);
         break;
-        case 2;
-        setVibration(250, 100);
+      case 2:
+        led(blue, light, 0);
+        setVibration(800, 80);
         break;
-        case 3:
+      case 3:
         vibrationSequence = 0;
         led(blue, noLight, 0);
+        LVL1Steps = 1;
+        Count_pulses = 0;
         hintIsfinished = true;
         break;
     }
+
+    //if user use it before hint is finished restart.
+    if (Count_pulses <= -50) {
+      //Position passed. Activate -> motor has to reset
+      led(blue, noLight, 0);
+      setVibration(0, 0);
+      LVL1Steps = 0;
+      vibrationSequence = -1;
+      motorFinishedReset = false;
+      userskippedHint = true;
+    }
   }
 
-  ///////// LEVEL RULES
+  /// idk if its necessary to check.
+  if (userskippedHint) {
+    hintIsfinished = true;
+    MotorTriggerCount = 0;
+    userskippedHint = false;
+  }
+
+  ///////// LEVEL 1
+
+  //if fail return to begin
+  if (!motorFinishedReset) {
+    //resetMotor, turn red light.
+    resetMotorTo(0, 100);
+    //step 0 is lvl1 hasnt begun -> restart
+    LVL1Steps = 0;
+  }
+
+
+
+  switch (LVL1Steps) {
+
+    case 1:
+      if (motorFinishedReset) {
+        if (isMoving(false) && Count_pulses <= -10 && Count_pulses > -400) {
+          //Position passed. Activate -> motor has to reset
+          led(green, light, 0);
+          //motorFinishedReset = false;
+        }
+        if (isMoving(false) && Count_pulses <= -301 && Count_pulses > -400) {
+          //Position passed. Activate -> motor has to reset
+          led(orange, light, 0);
+          //motorFinishedReset = false;
+        }
+        //minimal distance to travel before accesing next step
+        if (Count_pulses <= -250)
+          if (!isMoving(false)) {
+            if (!isStopforXsec(10000)) {
+              //led(green, light, 0);
+            } else {
+              led(green, light, 5);
+              Count_pulses = 0;
+              LVL1Steps = 2;
+            }
+          }
+        if (Count_pulses <= -401) {
+          motorFinishedReset = false;
+        }
+        //led(green, light, 2);
+      }
+      break;
+
+    case 2:
+
+      if (motorFinishedReset) {
+        if (isMoving(false) && Count_pulses <= -10 && Count_pulses > -200) {
+          //Position passed. Activate -> motor has to reset
+          led(green, light, 0);
+          //motorFinishedReset = false;
+        }
+        if (isMoving(false) && Count_pulses <= -150 && Count_pulses > -200) {
+          //Position passed. Activate -> motor has to reset
+          led(orange, light, 0);
+          //motorFinishedReset = false;
+        }
+        //minimal distance to travel before accesing next step
+        if (Count_pulses <= -120)
+          if (!isMoving(false)) {
+            if (!isStopforXsec(10000)) {
+              //led(green, light, 0);
+            } else {
+              led(green, light, 5);
+              Count_pulses = 0;
+              LVL1Steps = 3;
+            }
+          }
+        if (Count_pulses <= -201) {
+          motorFinishedReset = false;
+        }
+        //led(green, light, 2);
+      }
+
+
+
+      break;
+
+    case 3:
+
+      if (motorFinishedReset) {
+        if (isMoving(false) && Count_pulses <= -10 && Count_pulses > -400) {
+          //Position passed. Activate -> motor has to reset
+          led(green, light, 0);
+          //motorFinishedReset = false;
+        }
+        if (isMoving(false) && Count_pulses <= -301 && Count_pulses > -400) {
+          //Position passed. Activate -> motor has to reset
+          led(orange, light, 0);
+          //motorFinishedReset = false;
+        }
+        //minimal distance to travel before accesing next step
+        if (Count_pulses <= -250)
+          if (!isMoving(false)) {
+            if (!isStopforXsec(10000)) {
+              //led(green, light, 0);
+            } else {
+              led(green, light, 5);
+              Count_pulses = 0;
+              ///LVL 1 is FINISHED
+              LVL1Steps = 4;
+            }
+          }
+        if (Count_pulses <= -401) {
+          motorFinishedReset = false;
+        }
+        //led(green, light, 2);
+      }
+
+      break;
+  }
+
+
+
+
   /*
+
+  
+
+  
    if (Count_pulses <= -500) {
     //Position passed. Activate -> motor has to reset
     motorFinishedReset = false;
@@ -244,6 +404,7 @@ void ledManager(int color, bool On_Off, int blinkingTime) {
 
 
 
+
   //Serial.print(String("   Interval value:  ") + interval + String("    "));
 
   //if there is a blinking time go blink.
@@ -258,6 +419,7 @@ void ledManager(int color, bool On_Off, int blinkingTime) {
       digitalWrite(greenPin, LOW);
       digitalWrite(bluePin, LOW);
       ledshutdown = 0;
+
       color = NULL;
       lightled = false;
     }
@@ -307,38 +469,60 @@ void ledManager(int color, bool On_Off, int blinkingTime) {
         default:
 
           break;
+      }
+    }
+    //if not blink. Just turn it on or off
+  } else {
+
+    //just turn ON/OFF
+    switch (color) {
+      case 1:
+        Serial.print(String("  Red  ") + On_Off);
+
+        digitalWrite(orangePin, LOW);
+        digitalWrite(greenPin, LOW);
+        digitalWrite(bluePin, LOW);
+        ledtoLight = redPin;
+        ledstate = On_Off;
+        break;
+
+      case 2:
+        Serial.print(String("  Orange  ") + On_Off);
+        digitalWrite(redPin, LOW);
+
+
+        digitalWrite(bluePin, LOW);
+        ledtoLight = orangePin;
+        ledstate = On_Off;
+        break;
+
+      case 3:
+        Serial.print(String("  Green  ") + On_Off);
+        digitalWrite(redPin, LOW);
+
+
+        digitalWrite(bluePin, LOW);
+        ledtoLight = greenPin;
+        ledstate = On_Off;
+        break;
+
+      case 4:
+        Serial.print(String("  BLUE  ") + On_Off);
+
+        digitalWrite(redPin, LOW);
+        digitalWrite(orangePin, LOW);
+        digitalWrite(greenPin, LOW);
+
+        ledtoLight = bluePin;
+        ledstate = On_Off;
+        break;
     }
   }
-  //if not blink. Just turn it on or off
-}
-else {
-
-  //just turn ON/OFF
-  switch (color) {
-    case 1:
-      ledtoLight = redPin;
-      ledstate = On_Off;
-      break;
-
-    case 2:
-      ledtoLight = orangePin;
-      ledstate = On_Off;
-      break;
-
-    case 3:
-      ledtoLight = greenPin;
-      ledstate = On_Off;
-      break;
-
-    case 4:
-      Serial.print(String("  BLUE LIGHT  ") + On_Off);
-      ledtoLight = bluePin;
-      ledstate = On_Off;
-      break;
+  //blink desired desired led
+  digitalWrite(ledtoLight, ledstate);
+  if (!On_Off) {
+    lightled = false;
   }
-}
-//blink desired desired led
-digitalWrite(ledtoLight, ledstate);
 }
 
 void setVibration(int vibrationDuration, int vibrationForce) {
@@ -349,15 +533,22 @@ void setVibration(int vibrationDuration, int vibrationForce) {
 }
 
 void vibrationHint(int vibrationDuration, int vibrationForce) {
-  int interval = seconds();
+  //int interval = seconds();
   const long intervalue = vibrationDuration;
+  const long delayBeforeVibrating = 1000;
   int power;
   unsigned long currentMillis = millis();
 
+
+
   if (!delayIsPassed) {
-    if (currentMillis - VibratorpreviousMillis >= 500) {
+
+    power = 0;
+    if (currentMillis - VibratorpreviousMillis >= delayBeforeVibrating) {
+
       VibratorpreviousMillis = currentMillis;
       //half second has passed
+      Serial.print("PAAAAASSSEDDD");
       delayIsPassed = true;
     }
   } else {
@@ -367,7 +558,8 @@ void vibrationHint(int vibrationDuration, int vibrationForce) {
       power = 0;
       vibrationSequence++;
 
-      vibrate, delayIsPassed = false;
+      vibrate = false;
+      delayIsPassed = false;
 
     } else {
       power = vibrationForce;
@@ -379,7 +571,11 @@ void vibrationHint(int vibrationDuration, int vibrationForce) {
     vibrationSequence++;
     vibrate = false;
   }*/
-  Serial.print(String(" VIBRATING: ") + power);
+
+  if (vibrationDuration == 0) {
+    power = 0;
+  }
+  Serial.print(String(" VIBRATING: ") + power + String("delayIsPassed:  ") + delayIsPassed);
   analogWrite(vibratorPin, power);
 }
 
@@ -395,6 +591,7 @@ void resetMotorTo(int position, int speed) {
     led(red, light, 5);
     analogWrite(motorPin, 0);
     MotorTriggerCount++;
+    hintIsfinished = false;
     motorFinishedReset = true;
   }
 }
@@ -406,8 +603,43 @@ bool inRange(int position) {
 void DC_Motor_Position() {
   int b = digitalRead(Encoder_output_B);
   if (b > 0) {
+
     Count_pulses++;
   } else {
+    isMoving(true);
     Count_pulses--;
   }
+}
+
+bool isStopped;
+bool isStopforXsec(int millisec) {
+
+  unsigned long currentMillis = millis();
+
+  if (!isMoving(false)) {
+    if (currentMillis - IsStoppedpreviousSeconds >= 10000) {
+      IsStoppedpreviousSeconds = currentMillis;
+      isStopped = true;
+    }
+  } else {
+    isStopped = false;
+  }
+
+  return isStopped;
+}
+
+bool isMoving(bool calledfromMotor) {
+
+  unsigned long currentMillis = millis();
+
+  if (calledfromMotor) {
+    isMovingpreviousMillis = currentMillis;
+    ismoving = true;
+  } else {
+
+    if (currentMillis - isMovingpreviousMillis >= 1000) {
+      ismoving = false;
+    }
+  }
+  return ismoving;
 }
